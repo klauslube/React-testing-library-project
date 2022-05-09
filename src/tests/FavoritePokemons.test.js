64d477896dfdd50b959ca65e9e1d3cfda433859d
klauslube <klauslube93@gmail.com>
2022-05-09 17:35:34 -0300

    Requisito 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Teste o componente Favorite Pokemons', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/favorites');
+  const message = screen.getByText(/no favorite pokemon found/i, { selector: 'p' });
+  expect(message).toBeInTheDocument();
+
+  history.push('/');
+  const details = screen.getByRole('link', { name: /More details/i });
+  userEvent.click(details);
+  const label = screen.getByLabelText(/pokémon favoritado/i);
+  userEvent.click(label);
+  // const inputFavorite = screen.getbyRole('button', { checked: true });
+  history.push('/favorites');
+  const favorite = screen.getByText(/pikachu/i);
+  expect(favorite).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Teste o componente <NotFound.js />', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/notFound/');
+
+  const notFoundTitle = screen.getByRole('heading',
+    { name: /Page requested not found/i, level: 2 });
+  expect(notFoundTitle).toBeInTheDocument();
+
+  const notFoundImg = screen.getByAltText(
+    /Pikachu crying because the page requested was not found/i,
+  );
+  expect(notFoundImg).toBeInTheDocument();
+  expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
